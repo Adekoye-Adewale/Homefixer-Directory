5024d09133dc3eed21ab7bf7004f2979998de312
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-11-07 20:50:41 +0100

    Add blog categories to BlogArchivePage
    BlogArchivePage now fetches blog categories using getAllBlogsCategory and passes them to BlogPageComponent as a new prop.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import BlogPageComponent from '@/components/pages/blog'
-import { getAllBlogs } from '@/sanity/lib/client'
+import { getAllBlogs, getAllBlogsCategory } from '@/sanity/lib/client'
 
 export default async function BlogArchivePage() {
 
         const posts = await getAllBlogs()
-
-        // console.log("first log - Blog Page", posts)
+        const categories = await getAllBlogsCategory()
         
         return (
                 <BlogPageComponent
                         blogs={posts}
+                        categories={categories}
                 />
         )
 }
