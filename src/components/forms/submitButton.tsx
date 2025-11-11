b5498a19a2f5970b51332dec1c25fb7bdda11e6f
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-11-11 21:06:40 +0100

    Update submitButton.tsx

--- src/components/forms/submitButton.tsx
+++ src/components/forms/submitButton.tsx
@@ -5,7 +5,7 @@ export default function SubmitButton({ label }: { label: string }) {
         return (
                 <Button
                         type="submit"
-                        className="w-full mt-4 bg-black text-white border border-black hover:bg-black/10 hover:text-black transition-colors duration-300 text-sm cursor-pointer"
+                        className="w-full mt-4 bg-black text-white border rounded-sm border-black hover:bg-black/10 hover:text-black transition-colors duration-300 text-sm cursor-pointer"
                 >
                         {label}
                 </Button>
