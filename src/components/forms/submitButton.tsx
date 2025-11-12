3f5ee1c4176d9460b1b56e370787a62ddb6d0470
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-11-12 17:42:30 +0100

    Update submitButton.tsx

--- src/components/forms/submitButton.tsx
+++ src/components/forms/submitButton.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
 
-export default function SubmitButton({ label }: { label: string }) {
+type SubmitButtonProps = {
+        status: "idle" | "submitting" | "submitted"
+        label: string
+}
+
+export default function SubmitButton({ status = "idle", label }: SubmitButtonProps) {
         return (
                 <Button
                         type="submit"
+                        disabled={status === "submitting"}
                         className="w-full mt-4 bg-black text-white border rounded-sm border-black hover:bg-black/10 hover:text-black transition-colors duration-300 text-sm cursor-pointer"
                 >
-                        {label}
+                        {status === "submitting" ? (
+                                <span className='flex gap-1 items-center'>
+                                        <Loader2 className="size-4 animate-spin" /> 
+                                        <span>
+                                                Submitting...
+                                        </span>
+                                </span>
+                        ) : (
+                                `${ label }`
+                        )}
                 </Button>
         )
 }
