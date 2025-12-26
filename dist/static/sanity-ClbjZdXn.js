6baac08b82297e60a71d4a8913e79c441048d3d7
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-12-26 11:59:33 +0100

    update the sanity schema

---
+++ dist/static/sanity-ClbjZdXn.js
@@ -0,0 +1,1 @@
+import{r as t,s as e}from"./sanity.config-DvbbwI9h.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
---
+++ schema/static/sanity-olcXaXFG.js
@@ -0,0 +1,1 @@
+import{r as t,s as e}from"./sanity.config-BOzSZABL.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
